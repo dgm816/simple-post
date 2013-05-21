59705ec531395bf6b4b40209e469945b9b956658
Doug Miller <dgm816@gmail.com>
2013-05-21 16:45:32 -0500

    fixed system exit points, fixed encode function, added flags to yEncode for first/last line special handling, tested on sample yEnc file for correctness, wrote function to handle adding line breaks

--- post.py
+++ post.py
@@ -7,6 +7,7 @@ be fully implemented within a single python file.
 import re
 import socket
 import ssl
+import sys
 
 server = 'news.example.com'
 port = 119
@@ -106,49 +107,129 @@ def Login(s, username, password):
     # all went well, return true
     return True
 
-def yEncode(char):
+def yEncode(char, first=False, last=False):
     '''Encode one character using the yEnc algorithm.
     
     Return the yEnc encoded value and handle the special characters by appending
     the escape character and encoding alternatly. One or two characters will be
     returned each time this function is called.
+    
+    The 'first' argument has been designed so we encode the characters properly
+    if it is the first character of a new line. This will encode period/space/
+    tab characters.  Also, passing the 'first' flag inheriently will enable the
+    'last' flag (to pick up space/tab characters).
+    
+    The 'last' argument has been designed to encode the space/tab characters
+    properly when the last character of the line is being encoded.
     '''
+    
+    # check if special rules are being used
+    if first == True:
+        last = True
     
     # holds our output
     output = ''
     
     # encode the string with yEnc
-    encoded = (ord(char) + 42) % 256
+    e = (ord(char) + 42) % 256
     
     # check for special characters
-    if encoded == 0x00:
-        encoded = (ord(char) + 64) % 256
+    if e == 0x00:
+        e = (e + 64) % 256
         output = '='
-    elif encoded == 0x0a:
-        encoded = (ord(char) + 64) % 256
+    # the encoded 0x09 (tab char) was removed in version 1.2 of the yenc spec;
+    # however, we still should force to encode this character if it is the
+    # first or last character of a line
+    elif e == 0x09 and last == True:
+        e = (e + 64) % 256
         output = '='
-    elif encoded == 0x0d:
-        encoded = (ord(char) + 64) % 256
+    elif e == 0x0a:
+        e = (e + 64) % 256
         output = '='
-    elif encoded == 0x3d:
-        encoded = (ord(char) + 64) % 256
+    elif e == 0x0d:
+        e = (e + 64) % 256
+        output = '='
+    # the encoded 0x20 (space char) should be used it it is the first or the
+    # last character of a line
+    elif e == 0x20 and last == True:
+        e = (e + 64) % 256
+        output = '='
+    # the encoded 0x2e (period) only needs encoding on the first line to adhere
+    # to the nntp rfc
+    elif e == 0x2e and first == True:
+        e = (e + 64) % 256
+        output = '='
+    elif e == 0x3d:
+        e = (e + 64) % 256
         output = '='
     
     # append the encoded value to the output string
-    output += chr(encoded)
+    output += chr(e)
     
     # return the value
     return output
 
+def yEncodeData(data, chars=128):
+    '''Encode an entire data chunk obeying the formatting rules.
+    
+    '''
+    
+    # holds our output
+    line = ''
+    output = ''
+    count = 1
+    
+    # loop over data passed
+    for char in data:
+        # encode each character
+        if len(line) == 0:
+            line += yEncode(char, first=True)
+        elif len(line) == chars:
+            line += yEncode(char, last=True)
+        elif len(data) == count:
+            line += yEncode(char, last=True)
+        else:
+            line += yEncode(char)
+        
+        # check if we have a full line
+        if len(line) >= chars:
+            # save to output
+            output += line + "\n"
+            # clear the line
+            line = ''
+    
+    # check if we have a partial line to append
+    if len(line) > 0:
+        output += line + "\n"
+    
+    # return our encoded and formatted data
+    return output
+
 if __name__ == '__main__':
     
-    myFile = file('testfile.txt').read()
-    myEncoding = ''
+    header = '''From: develop@winews.net
+Newsgroups: yenc
+Date: 27 Oct 2001 15:07:44 +0200
+Subject: yEnc-Prefix: "testfile.txt" 584 yEnc bytes - yEnc test (1)
+Message-ID: <4407f.ra1200@liebchen.winews.net>
+Path: liebchen.winews.net!not-for-mail
+Lines: 16
+X-Newsreader: MyNews
+
+-- 
+=ybegin line=128 size=584 name=testfile.txt 
+'''
+
+    footer = '''=yend size=584 crc32=ded29f4f
+'''
+    myFile = file('testfile.txt', 'rb').read()
     
-    for char in myFile:
-        myEncoding += yEncode(char)
+    f = file("output.txt", "wb")
+    f.write(header)
+    f.write(yEncodeData(myFile))
+    f.write(footer)
     
-    quit
+    sys.exit()
     
     # connect to server
     conn = Connect(server, port, use_ssl)
@@ -156,12 +237,13 @@ if __name__ == '__main__':
     # check for failure
     if conn == None:
         print("Unable to connect to server.")
-        quit
+        sys.exit()
     
     # login to server
     if Login(conn, username, password) == None:
         print("Unable to login to server.")
-        quit
+        conn.close()
+        sys.exit()
     
     # quit server connection
     
